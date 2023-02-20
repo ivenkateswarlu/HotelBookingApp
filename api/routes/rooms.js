abdd9eda296752a9a9a46270297fcd89900cac1c
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2023-02-20 14:14:53 -0600

    Room functionality

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,28 @@
 import express from "express"
+import { createRoom,updatedRoom,deleteRoom,getRoom,getAllRooms } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//create
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//update
+
+router.put("/:id",verifyAdmin, updatedRoom);
+
+//Delete
+
+router.delete("/:id/:hotelid",verifyAdmin, deleteRoom);
+
+//Get
+
+router.get("/:id",getRoom);
+
+//Getall
+
+router.get("/", getAllRooms);
+
 
 
 export default router
